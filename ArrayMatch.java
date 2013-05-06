1f0d61026ce380bdbaf9c05abef31b06cde1d628
Amy <amyschlesener@gmail.com>
2013-05-05 17:17:09 -0700

    added elapsed time measurement (in nanoseconds)

--- ArrayMatch.java
+++ ArrayMatch.java
@@ -14,14 +14,29 @@ import java.io.*;
 	int size_b = 10;
 	int [] a = {2, 3, 5, 6, 8, 9, 10, 11, 12, 13};
   	int [] b = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+	int l;
+	//print elements of array a
+	System.out.print("Array a: ");
+	for(l = 0; l < size_a; l++) {
+		System.out.print(a[l]+" ");
+	}
+	System.out.println();
+	//print elements of array b
+	System.out.print("Array b: ");
+	for(l = 0; l < size_b; l++) {
+		System.out.print(b[l]+" ");
+	}	
+	System.out.println();
+	//commonArray will hold the shared elements of a and b
 	int[] commonArray;
-	 commonArray= new int[size_a+size_b];
+	commonArray= new int[size_a+size_b];
 	int sumDistinct = 0;
 	int i = 0;
 	int j = 0;
 	int k = 0;
+	long startTime = System.nanoTime();
 	while((i<a.length)&&(j<b.length)) {
-
+	  //common element found, add to commonArray
 	  if(a[i] == b[j]) {
 	    commonArray[k] = a[i];
 	    sumDistinct++;
@@ -36,7 +51,9 @@ import java.io.*;
 	    j++;
 	  }
 	}
-	System.out.println(sumDistinct);
-
+	
+	long estimatedTime = System.nanoTime() - startTime;
+	System.out.println("Number of values appearing in both arrays: " + sumDistinct);
+	System.out.println("Elapsed time in nanoseconds: " + estimatedTime);
   }
 }
